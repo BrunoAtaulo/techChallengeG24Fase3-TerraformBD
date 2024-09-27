3e0a789d0b5c2a09c778c68e368fb1d55e50adc8
Bruno Ataulo <b.begliomini@gmail.com>
2024-09-26 21:26:38 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,8 @@ provider "aws" {
 resource "aws_security_group" "rds_sg" {
   name = "rds_sg"
   ingress {
-    from_port       = 3306
-    to_port         = 3306
+    from_port       = 1433
+    to_port         = 1433
     protocol        = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -21,8 +21,8 @@ resource "aws_security_group" "rds_sg" {
 }
 
 #create a RDS Database Instance
-resource "aws_db_instance" "mysqltechchallengeDb" {
-  engine               = "mysql"
+resource "aws_db_instance" "sqlserver" {
+  engine               = "sqlserver"
   identifier           = "rdstechchallengeDb"
   allocated_storage    =  20
   engine_version       = "5.7"
