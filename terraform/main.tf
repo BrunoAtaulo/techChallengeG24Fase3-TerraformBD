3da18553ff8288abd875aa95998715e2fbcfbd19
Bruno Ataulo <b.begliomini@gmail.com>
2024-09-28 20:19:03 -0300

    Atualizado

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_db_instance" "sqltechchallengeDb" {
   engine_version         = "14.00.3475.1.v1"
   instance_class          = "db.t3.micro"
   username                = "SA"
-  password                = ${{ secrets.BD_PASSWORD }}
+  password                = "${{ secrets.BD_PASSWORD }}"
   parameter_group_name    = "sqlserver-ex-14"
   vpc_security_group_ids  = ["${aws_security_group.rds_secgrptechchallenge.id}"]
   skip_final_snapshot     = true
